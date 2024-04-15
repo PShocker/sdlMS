41980f189102f2549c61fb679ba23a6f510ab6ac
pengxiongke <491061744@qq.com>
2024-04-15 20:58:53 +0800

    背景优化

--- src/sdlms/camera.cpp
+++ src/sdlms/camera.cpp
@@ -12,45 +12,36 @@ void Camera::update(int elapsedTime)
     Point<float> player_pos = _character->_pos - Point<float>{_viewport.w / 2, _viewport.h / 2};
     Point<float> camera_pos{_viewport.x, _viewport.y};
 
-    _viewport.x = player_pos.x();
-    _viewport.y = player_pos.y();
-    return;
-    // int VWIDTH = _viewport.w;
-    // int VHEIGHT = _viewport.h;
+    auto hdelta = player_pos.x() - camera_pos.x();
+    if (std::abs(hdelta) >= 5.0)
+    {
+        _viewport.x += hdelta * (12.0 / _viewport.w);
+    }
 
-    // double next_x = _viewport.x;
-    // double hdelta = VWIDTH / 2 - player_pos.x() - next_x;
+    auto vdelta = player_pos.y() - camera_pos.y();
+    if (std::abs(vdelta) >= 5.0)
+    {
+        _viewport.y += vdelta * (12.0 / _viewport.h);
+    }
 
-    // if (std::abs(hdelta) >= 5.0)
-    //     next_x += hdelta * (12.0 / VWIDTH);
+    auto [Left, Right, Top, Bottom] = Map::current()->_border;
 
-    // double next_y = _viewport.y;
-    // double vdelta = VHEIGHT / 2 - player_pos.y() - next_y;
+    if (_viewport.x < Left)
+    {
+        _viewport.x = Left;
+    }
+    else if (_viewport.x + _viewport.w > Right)
+    {
+        _viewport.x = Right - _viewport.w;
+    }
 
-    // if (std::abs(vdelta) >= 5.0)
-    //     next_y += vdelta * (12.0 / VHEIGHT);
-
-    // auto [Left, Right, Top, Bottom] = Map::current()->_border;
-
-    // if (next_x < Left)
-    // {
-    //     next_x = Left;
-    // }
-    // else if (next_x + VWIDTH > Right)
-    // {
-    //     next_x = Right - VWIDTH;
-    // }
-
-    // if (next_y < Top)
-    // {
-    //     next_y = Top;
-    // }
-    // else if (next_y + VHEIGHT > Bottom)
-    // {
-    //     next_y = Bottom - VHEIGHT;
-    // }
-    // _viewport.x = next_x;
-    // _viewport.y = next_y;
-
+    if (_viewport.y < Top)
+    {
+        _viewport.y = Top;
+    }
+    else if (_viewport.y + _viewport.h > Bottom)
+    {
+        _viewport.y = Bottom - _viewport.h;
+    }
     return;
 }
