21fc478b986cd7c000bfa9803999ce4a6dde8550
Shocker <pengxk@chinatelecom.cn>
2024-05-14 16:41:18 +0800

    update

--- sdlms/Entities/LadderRope.cpp
+++ sdlms/Entities/LadderRope.cpp
@@ -1,5 +1,6 @@
 #include "LadderRope.h"
 #include "Components/CrawlLine.h"
+#include "Components/Physic/Normal.h"
 
 LadderRope::LadderRope(wz::Node *node, int id, World *world) : id(id)
 {
@@ -17,6 +18,21 @@ LadderRope::LadderRope(wz::Node *node, int id, World *world) : id(id)
 
 LadderRope::~LadderRope()
 {
+    // 摧毁lp,需要删除lp关联到ent的map
+    auto world = World::get_world();
+    if (world->components_exist_of_type<Normal>())
+    {
+        // 删除所有nor的ent关联到lp的map
+        for (auto &[key, val] : world->get_components<Normal>())
+        {
+            auto nor = val;
+            if (nor->get_owner()->get_entity<LadderRope>() == this)
+            {
+                nor->get_owner()->remove_entity<LadderRope>();
+                nor->type = Normal::Air;
+            }
+        }
+    }
     auto c = get_component<CrawlLine>();
     delete c;
 }
