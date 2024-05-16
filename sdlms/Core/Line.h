dfbfa4b8e2a31d1694d94eb1d43c0a5175ca2ece
Shocker <pengxk@chinatelecom.cn>
2024-05-16 14:48:15 +0800

    update

--- sdlms/Core/Line.h
+++ sdlms/Core/Line.h
@@ -9,10 +9,10 @@ public:
     Line(SDL_FPoint m, SDL_FPoint n);
     constexpr auto get_m() { return m; }
     constexpr auto get_n() { return n; }
-    constexpr auto get_min_x() { return std::min(m.x, n.x); }
-    constexpr auto get_max_x() { return std::max(m.x, n.x); }
-    constexpr auto get_min_y() { return std::min(m.y, n.y); }
-    constexpr auto get_max_y() { return std::max(m.y, n.y); }
+    constexpr auto get_min_x() { return std::fmin(m.x, n.x); }
+    constexpr auto get_max_x() { return std::fmax(m.x, n.x); }
+    constexpr auto get_min_y() { return std::fmin(m.y, n.y); }
+    constexpr auto get_max_y() { return std::fmax(m.y, n.y); }
     constexpr auto get_k() { return k; }
     std::optional<float> get_y(float x);
     std::optional<float> get_x(float y);
