34bcf25231d0f995a680d9fde732962249f2b93d
Shocker <pengxk@chinatelecom.cn>
2024-06-03 12:41:38 +0800

    update

--- sdlms/Entities/Border.cpp
+++ sdlms/Entities/Border.cpp
@@ -15,23 +15,34 @@ Border::Border(wz::Node *node, World *world)
     }
     else
     {
-        // 从fh计算出来
-        for (auto &[id, fh] : world->get_entitys<FootHold>())
+        if (world->entity_exist_of_type<FootHold>())
         {
-            auto rl = fh->get_component<RigidLine>();
-            auto x1 = rl->get_m().x;
-            auto y1 = rl->get_m().y;
-            auto x2 = rl->get_n().x;
-            auto y2 = rl->get_n().y;
-            left = std::min({left, x1, x2});
-            right = std::max({right, x1, x2});
-            top = std::min({top, y1, y2});
-            bottom = std::max({bottom, y1, y2});
+            // 从fh计算出来
+            for (auto &[id, fh] : world->get_entitys<FootHold>())
+            {
+                auto rl = fh->get_component<RigidLine>();
+                auto x1 = rl->get_m().x;
+                auto y1 = rl->get_m().y;
+                auto x2 = rl->get_n().x;
+                auto y2 = rl->get_n().y;
+                left = std::min({left, x1, x2});
+                right = std::max({right, x1, x2});
+                top = std::min({top, y1, y2});
+                bottom = std::max({bottom, y1, y2});
+            }
+            left += 25;
+            right -= 25;
+            top -= 300;
+            bottom += 100;
         }
-        left += 25;
-        right -= 25;
-        top -= 300;
-        bottom += 100;
+        else
+        {
+            // 该地图不存在fh
+            left = -10000;
+            right = 10000;
+            top = -10000;
+            bottom = 10000;
+        }
     }
 }
 
