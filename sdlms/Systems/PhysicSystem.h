864b8e55019743df6fa8bf04b2ac34352d11c402
Shocker <491061744@qq.com>
2024-06-06 12:18:35 +0800

    update

--- sdlms/Systems/PhysicSystem.h
+++ sdlms/Systems/PhysicSystem.h
@@ -19,7 +19,8 @@ private:
 	bool want_portal(Transform *tr, Normal *nor, World &world);
 	bool walk(Transform *tr, Normal *nor, World &world, float delta_time);
 	void fall(Transform *tr, Normal *nor, float delta_time, World &world);
-	void climb(Transform *tr, Normal *nor,float delta_time);
+	void climb(Transform *tr, Normal *nor, float delta_time);
+	void limit(Transform *tr, World &world);
 	inline std::optional<SDL_FPoint> intersect(SDL_FPoint p1,
 											   SDL_FPoint p2,
 											   SDL_FPoint p3,
