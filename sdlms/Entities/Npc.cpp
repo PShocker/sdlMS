c99363f4968be8aabf923f992431f0ec8a348e2b
Shocker <491061744@qq.com>
2024-06-16 19:38:03 +0800

    update

--- sdlms/Entities/Npc.cpp
+++ sdlms/Entities/Npc.cpp
@@ -84,17 +84,17 @@ Npc::Npc(wz::Node *node, int id, int rx0, int rx1, World *world)
                     add_entity(str);
                     auto spr = str->get_component<Sprite>();
                     {
-                        auto rtr = new RelativeTransform(tr, SDL_FPoint{(float)(-spr->get_width() / 2 + 2), (float)(9 + (spr->get_height() + 6) * i)});
-                        str->add_component(rtr);
-                        str->add_component(new Transform());
-                        world->add_component(rtr, 1);
-                    }
-                    {
                         auto nam = new NameTag(spr->width + 4, spr->height + 6);
                         add_entity(nam);
                         auto rtr = new RelativeTransform(tr, SDL_FPoint{(float)(-spr->get_width() / 2), (float)(6 + (spr->get_height() + 6) * i)});
                         nam->add_component(rtr);
                         nam->add_component(new Transform());
+                        world->add_component(rtr, 0);
+                    }
+                    {
+                        auto rtr = new RelativeTransform(tr, SDL_FPoint{(float)(-spr->get_width() / 2 + 2), (float)(9 + (spr->get_height() + 6) * i)});
+                        str->add_component(rtr);
+                        str->add_component(new Transform());
                         world->add_component(rtr, 0);
                     }
                     i++;
