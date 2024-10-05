fd8eeddb92a30b20e080a818894274ee63a0c69f
Shocker <491061744@qq.com>
2024-10-05 21:55:05 +0800

    update

--- sdlms/Systems/PlayerStateMachine.cpp
+++ sdlms/Systems/PlayerStateMachine.cpp
@@ -549,6 +549,8 @@ bool player_jump(Move *mv, Character *cha, Transform *tr, int state)
 
                 SkillWarp::cooldowns[u"4111006"] = 300;
 
+                Sound::sound_list.push_back(Sound(u"Game.img/Jump"));
+
                 return true;
             }
         }
