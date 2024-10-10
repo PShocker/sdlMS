6865830222bb5727ae01bbd125c61dcaa9750c0e
Shocker <491061744@qq.com>
2024-10-10 20:46:46 +0800

    update

--- sdlms/main.cpp
+++ sdlms/main.cpp
@@ -52,7 +52,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     Sound::init();
     Drop::init();
 
-    World::load_map(200000000);
+    World::load_map(211000000);
 
     Player::ent = World::registry->create();
     load_character(0, 0, true, &Player::ent);
