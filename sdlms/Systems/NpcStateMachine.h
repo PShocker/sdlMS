db0c4130601e89e94852c33836bc66f45266b8eb
Shocker <491061744@qq.com>
2025-02-18 23:19:04 +0800

    update

--- sdlms/Systems/NpcStateMachine.h
+++ sdlms/Systems/NpcStateMachine.h
@@ -7,4 +7,4 @@ void npc_statemachine_run();
 
 void npc_statemachine(entt::entity ent, float delta_time);
 
-int npc_move(Move *mv, Transform *tr, float delta_time);
+void npc_move(Move *mv, Transform *tr, float delta_time);
