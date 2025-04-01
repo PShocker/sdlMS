e872a27b5c6ca0861b40e54adc9240c5d09102e2
Shocker <491061744@qq.com>
2025-04-01 21:57:04 +0800

    update

--- sdlms/Core/FreeType.h
+++ sdlms/Core/FreeType.h
@@ -14,6 +14,5 @@ struct FreeType
     static void renderGlyphToTexture(SDL_Texture *texture, char16_t c,
                                      SDL_Color color, int offsetX, int offsetY);
     static SDL_Texture *load(const std::u16string &s, SDL_Color color, int w);
-    static SDL_Texture *load(const std::string &s, SDL_Color color, int w);
     static void size(int size);
 };
